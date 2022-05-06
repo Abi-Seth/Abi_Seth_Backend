27974fde674586f1e2b0b741a8c7f5440f80b005
abi-seth <abiheloaf@gmail.com>
2022-05-06 15:52:20 +0200

    [fix]: made some fixes

--- validators/articles.validator.js
+++ validators/articles.validator.js
@@ -4,8 +4,8 @@ exports.validateArticleRegisteration = (body) => {
     const validArticleRegisterSchema = Joi.object({
         articleTitle: Joi.string().required().max(150).min(2),
         articleDescription: Joi.string().min(5).max(500).required(),
-        articleContent: Joi.string().min(5).max(5000).required(),
-        articleMainImage: Joi.string().min(5).max(500).required()
+        articleContent: Joi.string().min(5).max(50000).required(),
+        articleMainImage: Joi.string().min(5).max(5000000).required()
     })
     return validArticleRegisterSchema.validate(body);
 }
