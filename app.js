cc668fa6855193e1aa37253e92d7c8937a249511
abi-seth <abiheloaf@gmail.com>
2022-05-06 16:41:19 +0200

    fixed origins"

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const { messageRouter } = require('./routes/messages.routes');
 const { projectRouter } = require('./routes/projects.routes');
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Origin', 'https://abiseth.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 });
